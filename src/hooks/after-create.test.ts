9f36a5df22b08afc06fcc136e9b049b80ae635e2
sor4chi <sorakawa0218@gmail.com>
2023-10-14 15:14:36 +0900

    test: fix test for new hook style

--- src/hooks/after-create.test.ts
+++ src/hooks/after-create.test.ts
@@ -1,23 +1,13 @@
-import * as path from 'path'
+import { join } from 'path'
 import { describe, expect, it, vi } from 'vitest'
-import {
-  AFTER_CREATE,
-  addAfterCreateHook,
-  rewriteWranglerHook,
-} from './after-create'
+import { afterCreateHook } from './after-create'
 
-describe('addAfterCreateHook', () => {
-  it('adds a hook to the AFTER_CREATE map', () => {
-    const hook = () => {}
-    addAfterCreateHook('test-template', hook)
-    expect(AFTER_CREATE.get('test-template')).toEqual([hook])
-  })
-})
-
-describe('rewriteWranglerHook', () => {
-  it('rewrites the wrangler.toml file with the project name', async () => {
-    vi.mock('fs', () => {
-      const wrangler = `
+describe('afterCreateHook', () => {
+  describe('cloudflare-workers', () => {
+    describe('rewriteWranglerHook', () => {
+      it('rewrites the wrangler.toml file with the project name', async () => {
+        vi.mock('fs', () => {
+          const wrangler = `
 name = "%%PROJECT_NAME%%"
 type = "javascript"
 account_id = "test-account-id"
@@ -26,17 +16,17 @@ route = ""
 zone_id = ""
       `.trim()
 
-      return {
-        readFileSync: vi.fn().mockReturnValue(wrangler),
-        writeFileSync: vi.fn(),
-      }
-    })
-    const { readFileSync, writeFileSync } = await import('fs')
+          return {
+            readFileSync: vi.fn().mockReturnValue(wrangler),
+            writeFileSync: vi.fn(),
+          }
+        })
+        const { readFileSync, writeFileSync } = await import('fs')
 
-    const projectName = 'test-project'
-    const directoryPath = './tmp'
-    const wranglerPath = path.join(directoryPath, 'wrangler.toml')
-    const replaced = `
+        const projectName = 'test-project'
+        const directoryPath = './tmp'
+        const wranglerPath = join(directoryPath, 'wrangler.toml')
+        const replaced = `
 name = "${projectName}"
 type = "javascript"
 account_id = "test-account-id"
@@ -44,8 +34,13 @@ workers_dev = true
 route = ""
 zone_id = ""
     `.trim()
-    rewriteWranglerHook({ projectName, directoryPath })
-    expect(readFileSync).toHaveBeenCalledWith(wranglerPath, 'utf-8')
-    expect(writeFileSync).toHaveBeenCalledWith(wranglerPath, replaced)
+        afterCreateHook.applyHook('cloudflare-workers', {
+          projectName,
+          directoryPath,
+        })
+        expect(readFileSync).toHaveBeenCalledWith(wranglerPath, 'utf-8')
+        expect(writeFileSync).toHaveBeenCalledWith(wranglerPath, replaced)
+      })
+    })
   })
 })
