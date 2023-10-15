4312a2b42583af54a0426cbdb752adec38d6c15e
Yusuke Wada <yusuke@kamawada.com>
2023-10-16 07:55:21 +0900

    feat: regexp for project name & array template names for `addHook` (#11)

--- src/hooks/after-create.ts
+++ src/hooks/after-create.ts
@@ -1,22 +1,15 @@
 import { readFileSync, writeFileSync } from 'fs'
 import * as path from 'path'
-import { Hook } from '../hook'
+import { afterCreateHook } from '../hook'
 
-type AfterHookOptions = {
-  projectName: string
-  directoryPath: string
-}
-
-type AfterHookFunction = (options: AfterHookOptions) => void
-
-const afterCreateHook = new Hook<AfterHookFunction>()
+const PROJECT_NAME = new RegExp(/%%PROJECT_NAME.*%%/g)
 
 afterCreateHook.addHook(
   'cloudflare-workers',
   ({ projectName, directoryPath }) => {
     const wranglerPath = path.join(directoryPath, 'wrangler.toml')
     const wrangler = readFileSync(wranglerPath, 'utf-8')
-    const rewritten = wrangler.replaceAll('%%PROJECT_NAME%%', projectName)
+    const rewritten = wrangler.replaceAll(PROJECT_NAME, projectName)
     writeFileSync(wranglerPath, rewritten)
   }
 )
