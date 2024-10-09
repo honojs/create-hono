7323a5529b63ea0059c08b307e10c66f59a9347c
ryu <114303361+ryuapp@users.noreply.github.com>
2024-10-10 00:36:39 +0900

    chore: add Deno to current package manager candidates

--- src/hooks/dependencies.ts
+++ src/hooks/dependencies.ts
@@ -117,6 +117,7 @@ function getCurrentPackageManager(): PackageManager {
   const agent = process.env.npm_config_user_agent || 'npm' // Types say it might be undefined, just being cautious;
 
   if (agent.startsWith('bun')) return 'bun'
+  if (agent.startsWith('deno')) return 'deno'
   if (agent.startsWith('pnpm')) return 'pnpm'
   if (agent.startsWith('yarn')) return 'yarn'
 
