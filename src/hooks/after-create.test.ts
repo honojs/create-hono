7f5893cce561103530602646380752a0905a8231
Yusuke Wada <yusuke@kamawada.com>
2025-01-30 19:45:11 +0900

    fix: replace `compatibility_date` for `wrangler.json`

--- src/hooks/after-create.test.ts
+++ src/hooks/after-create.test.ts
@@ -4,29 +4,42 @@ import { afterCreateHook } from './after-create'
 
 describe('afterCreateHook', () => {
   describe('cloudflare-workers', () => {
-    describe('rewriteWranglerHook', () => {
-      it('rewrites the wrangler.toml file with the project name', async () => {
-        vi.mock('fs', () => {
-          const wrangler = `
+    describe('rewriteWranglerHook', async () => {
+      vi.mock('fs', () => {
+        const directoryPath = './tmp'
+        const mockFiles: Record<string, string> = {
+          [join(directoryPath, 'wrangler.toml')]: `
 name = "%%PROJECT_NAME%%"
 compatibility_date = "2023-12-01"
 
 [env.staging]
-name = "%%PROJECT_NAME%%-staging"
-      `.trim()
+name = "%%PROJECT_NAME%%-staging"`.trim(),
+          [join(directoryPath, 'wrangler.json')]: `
+"name": "%%PROJECT_NAME%%",
+"compatibility_date": "2023-12-01",
+"env": {
+  "staging": {
+  "name": "%%PROJECT_NAME%%-staging"
+}`.trim(),
+        }
 
-          return {
-            readFileSync: vi.fn().mockReturnValue(wrangler),
-            writeFileSync: vi.fn(),
-          }
-        })
-        const { readFileSync, writeFileSync } = await import('node:fs')
+        return {
+          readFileSync: vi.fn((path: string) => {
+            if (mockFiles[path]) {
+              return mockFiles[path]
+            }
+          }),
+          writeFileSync: vi.fn(),
+        }
+      })
 
-        const projectName = 'test-projectNAME+123'
-        const directoryPath = './tmp'
-        const wranglerPath = join(directoryPath, 'wrangler.toml')
-        const packageManager = 'npm'
+      const { readFileSync, writeFileSync } = await import('node:fs')
 
+      const projectName = 'test-projectNAME+123'
+      const directoryPath = './tmp'
+      const packageManager = 'npm'
+
+      it('rewrites the wrangler.toml file with the project name', async () => {
         const firstHookContent = `
 name = "test-projectname-123"
 compatibility_date = "2023-12-01"
@@ -45,13 +58,47 @@ compatibility_date = "${currentDate}"
 name = "%%PROJECT_NAME%%-staging"
     `.trim()
 
+        const wranglerPath = join(directoryPath, 'wrangler.toml')
+
         afterCreateHook.applyHook('cloudflare-workers', {
           projectName,
           directoryPath,
           packageManager,
         })
         expect(readFileSync).toHaveBeenCalledWith(wranglerPath, 'utf-8')
+
         expect(writeFileSync).nthCalledWith(1, wranglerPath, firstHookContent)
+        expect(writeFileSync).nthCalledWith(3, wranglerPath, secondHookContent)
+      })
+
+      it('rewrites the wrangler.json file with the project name', async () => {
+        const firstHookContent = `
+"name": "test-projectname-123",
+"compatibility_date": "2023-12-01",
+"env": {
+  "staging": {
+  "name": "test-projectname-123-staging"
+}`.trim()
+
+        // Get current date in YYYY-MM-DD format
+        const currentDate = new Date().toISOString().split('T')[0]
+        const secondHookContent = `
+"name": "%%PROJECT_NAME%%",
+"compatibility_date": "${currentDate}",
+"env": {
+  "staging": {
+  "name": "%%PROJECT_NAME%%-staging"
+}`.trim()
+
+        const wranglerPath = join(directoryPath, 'wrangler.json')
+
+        afterCreateHook.applyHook('cloudflare-workers', {
+          projectName,
+          directoryPath,
+          packageManager,
+        })
+        expect(readFileSync).toHaveBeenCalledWith(wranglerPath, 'utf-8')
+        expect(writeFileSync).nthCalledWith(2, wranglerPath, firstHookContent)
         expect(writeFileSync).nthCalledWith(4, wranglerPath, secondHookContent)
       })
     })
